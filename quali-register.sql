93ad32427f9886447345647171702bd5a17b5d27
brenelga <123479074+brenelga@users.noreply.github.com>
2023-08-13 17:11:53 -0600

    Actualizar quali-register.sql

--- quali-register.sql
+++ quali-register.sql
@@ -4,20 +4,21 @@
 Create database qualiregister
 
 Create table coordinador(
-id_coor int Not Null Primary Key,
+id_coor serial Not Null Primary Key,
 nombre_coor varchar(50),
 ap_pat_coor varchar(50),
 ap_mat_coor varchar(50)
 );
 
 Create Table asignatura(
-id_asig int Not Null Primary Key,
+id_asig serial Not Null Primary Key,
 nombre_asig varchar(75) Not Null,
 grado int Not Null
 );
 
 Create Table docente(
-numero_trabajador bigint Not Null Primary Key,
+iddoc serial Not Null Primary Key,
+numero_trabajador bigint Not Null unique,
 nombre_docente varchar(50) Not Null,
 ap_pat_doc varchar(50) Not Null,
 ap_mat_doc varchar(50) Not null,
@@ -25,12 +26,17 @@ contrasena varchar(15) Not Null
 );
 
 Create Table administrador(
-usuario varchar(30) not null Primary Key,
+idadmin serial not Null Primary Key,
+usuario varchar(30) not null unique,
+nombread varchar(30) not Null,
+appat varchar (30) not Null,
+appat varchar (30),
 constrasena_admin varchar (15) not null
 )
 
 Create Table alumnos(
-matricula bigint not Null Primary Key,
+idal serial not Null Primary Key,
+matricula bigint not Null unique,
 nombre varchar (50) not Null,
 apellido_paterno varchar(50) Not Null,
 apellido_materno varchar(50),
@@ -41,17 +47,17 @@ Foreign Key ("carrera") references "carrera"("id_carrera")
 );
 
 Create Table asistencia(
-id_Asistencia int Not Null Primary Key,
+id_Asistencia serial Not Null Primary Key,
 matricula bigint Not Null,
-fecha Date Not Null,
-asistencia Bool Not Null,
+fecha Date Not Null set default "current_date",
+asistencia double Not Null check asistencia >=0 and asistencia<=1,
 asignatura int Not Null,
 Foreign Key ("matricula") references "alumnos"("matricula"),
 Foreign Key ("asignatura") references "asignatura"("id_asig")
 );
 
 Create table tareas(
-id_tarea int Not Null Primary Key,
+id_tarea serial Not Null Primary Key,
 nombre_tarea varchar(35) Not Null,
 asignatura int Not Null,
 matricula bigint Not Null,
@@ -60,27 +66,27 @@ Foreign Key ("matricula") references "alumnos"(matricula)
 );
 
 Create table calificacion(
-id_calificacion int Not Null Primary Key,
+id_calificacion serial Not Null Primary Key,
 tarea int Not null,
 matricula bigint Not Null,
 asignatura int Not Null,
-calificacion float Not Null,
+calificacion float Not Null check calificacion <=100,
 Foreign Key ("tarea") references "tareas"(id_tarea),
 Foreign Key ("matricula") references "alumnos"("matricula"),
 Foreign Key ("asignatura") references "asignatura"("id_asig")
 );
 
 Create table "carrera"(
-id_carrera int Not Null Primary Key,
-nombre_carrera varchar(100) Not Null,
-coordinador int Not Null,
+id_carrera serial Not Null Primary Key,
+nombre_carrera varchar(100) Not Null unique,
+coordinador int Not Null unique,
 Foreign Key ("coordinador") references coordinador("id_coor")
 );
 
 Create table docente_asignatura(
-numero_trabajador bigint Not null,
+iddoc int Not null,
 id_asig int not null,
-Foreign key (numero_trabajador) references docente (numero_trabajador),
+Foreign key (iddoc) references docente (iddoc),
 Foreign Key (id_asig) references asignatura (id_asig)
 );
 
@@ -91,10 +97,4 @@ Foreign Key (matricula) references alumnos (matricula),
 Foreign key (id_asig) references asignatura (id_asig)
 );
 
-Alter table administrador ADD nombre varchar(50) Not Null;
-Alter table administrador ADD apellido_pat varchar(50) Not Null;
-Alter table administrador ADD apellido_mat varchar(50);
-Alter table asignatura ADD CONSTRAINT nombre_asig UNIQUE(nombre_asig);
-Alter table carrera ADD CONSTRAINT nombre_carrera UNIQUE (nombre_carrera);
-Alter table asistencia ALTER COLUMN fecha SET DEFAULT(current_date);
-Alter table asistencia ADD CONSTRAINT asistencia CHECK (asistencia >=0 OR asistencia <= 0);
+
